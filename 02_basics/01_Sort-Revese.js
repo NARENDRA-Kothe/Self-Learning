5dea4b025edf9d724d3fc8ce3d810ee162a128c3
NARENDRA-Kothe <115572547+NARENDRA-Kothe@users.noreply.github.com>
2024-01-21 09:28:07 +0000

    sort

---
+++ 02_basics/01_Sort-Revese.js
@@ -0,0 +1,19 @@
+// sort the elements of an array in place and return the sorted aaray.
+
+let sortArray = [12,10,5,6,34,21,34,15,2,4,7,24]
+
+sortArray.sort()
+
+console.log(sortArray);
+
+
+
+// reverse the order of the element in an array.
+
+let reverseAry = ["A","B","C"]
+
+reverseAry.reverse()
+
+console.log(reverseAry);
+
+
