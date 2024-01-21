af62233e50168402694da0cbcdf75f3b79d29fa1
NARENDRA-Kothe <115572547+NARENDRA-Kothe@users.noreply.github.com>
2024-01-21 13:42:03 +0000

    push method

---
+++ 02_basics/02_Push-Pop.js
@@ -0,0 +1,13 @@
+
+//Push Add last Value in Array
+
+let pushed = [1,2,3,4,5]
+pushed.push(6,7,8)
+console.log(pushed);
+
+
+//Pop Remove last Value in Array
+
+let popArray = [1,2,3,4,5,6]
+popArray.pop()
+console.log(popArray);
