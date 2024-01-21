72177aef8a0ef3c14f82988389d38c081304146f
NARENDRA-Kothe <115572547+NARENDRA-Kothe@users.noreply.github.com>
2024-01-21 14:32:34 +0000

    indexof

---
+++ 02_basics/05_indexOf.js
@@ -0,0 +1,6 @@
+
+/* The indexOf value find element indexing of Array or If it does not 
+find any value then the answer is -1*/
+
+let indexofArray = [1,2,3,4,5,6,7]
+console.log(indexofArray.indexOf(1));
