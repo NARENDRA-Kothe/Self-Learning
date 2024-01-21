ae229066dd72bd7f04ef0a866f7b96d702409c68
NARENDRA-Kothe <115572547+NARENDRA-Kothe@users.noreply.github.com>
2024-01-21 13:51:49 +0000

    shift

--- 02_basics/01_Sort-Revese.js
+++ 02_basics/01_Sort-Revese.js
@@ -1,9 +1,8 @@
+
 // sort the elements of an array in place and return the sorted aaray.
 
 let sortArray = [12,10,5,6,34,21,34,15,2,4,7,24]
-
 sortArray.sort()
-
 console.log(sortArray);
 
 
@@ -11,9 +10,7 @@ console.log(sortArray);
 // reverse the order of the element in an array.
 
 let reverseAry = ["A","B","C"]
-
 reverseAry.reverse()
-
 console.log(reverseAry);
 
 
---
+++ 02_basics/03_Unshift-Shift.js
@@ -0,0 +1,10 @@
+
+//Unshift Add First value in Array.
+let unshiftArray = [4,5,6,7,8]
+unshiftArray.unshift(1,2,3)
+console.log(unshiftArray);
+
+//Unshift Remove First value in Array.
+let shiftArray = [1,2,3,4,5,6,7,8,9]
+shiftArray.shift()
+console.log(shiftArray);
