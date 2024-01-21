244392b674b4b7e689deb8befa4f330c5040fb6a
NARENDRA-Kothe <115572547+NARENDRA-Kothe@users.noreply.github.com>
2024-01-21 14:24:36 +0000

    includes

---
+++ 02_basics/04_Includes.js
@@ -0,0 +1,5 @@
+/* Inclue is use to search particular value in array if the value present
+it return true if not then false */
+
+let includesArray = [1,2,3,4,5,6,8,9,0]
+console.log(includesArray.includes(3));
