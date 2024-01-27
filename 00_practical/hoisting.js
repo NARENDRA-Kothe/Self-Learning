7d6094f7f12eb86e88665bbd8ab9b183710a6c47
NARENDRA-Kothe <115572547+NARENDRA-Kothe@users.noreply.github.com>
2024-01-27 06:24:23 +0000

    change-2

---
+++ 00_practical/hoisting.js
@@ -0,0 +1,6 @@
+
+a = 10; // defined 
+console.log(a)
+var a; // declared
+// Ans.: a variable can be used before it has been declared.
+// Hoisting not working for Let and const
--- 00_practical/this.js
+++ 00_practical/this.js
@@ -1,11 +1,45 @@
+
 // this keyword is refer for current context.
 
-let userDetails = {
-name: "Narendra",
-price: 999,
+// this keyword does not work for arrow function.
 
-welcome: function(){
-    console.log(`${this.name} welcome to our website`)
+//***********-below example-1-**********
+
+// let userDetails = {
+// name: "Narendra",
+// price: 999,
+
+// welcome: function(){
+//     console.log(`${this.name} welcome to our website`)
+// }
+// }
+// userDetails.welcome()
+
+
+//***********-below example-2-**********
+
+/* When used in an object method, this refers to the object.
+In the example on below of this page, this refers to the person object.
+Because the fullName method is a method of the person object. */
+
+const person = {
+    firstName   : "Narendra",
+    lastName    : "Kothe",
+    phone       : 1234567890,
+    getFullName : function() {
+      return ' First Name:' + this.firstName + ', Last Name:' + this.lastName + ', Phone: ' + this.phone;
+    }
+};
+
+console.log('this in object method', person.getFullName());
+
+//***********-below example-3-**********
+
+// if we try to implement an arrow function to it as an object method, we won't be able to access the object through the this keyword: 
+const user = {
+    name: 'Pedro',
+    surname: 'Sanchez',
+    sayName: () => this.name + ' ' + this.surname
 }
-}
-userDetails.welcome()
+
+console.log(user.sayName());
