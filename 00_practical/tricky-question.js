dcf2d38a6c3c19ae01c280dddeeaa99572fdf3a6
NARENDRA-Kothe <115572547+NARENDRA-Kothe@users.noreply.github.com>
2024-02-01 11:54:56 +0000

    que

---
+++ 00_practical/tricky-question.js
@@ -0,0 +1,35 @@
+console.log ("8"+"5"); // 85 (string + string = string)
+
+console.log ("8"-"5"); // 3 (string - string)= number
+
+console.log (5 + 2 + "3"); // 73 (number + number + string)= add+string
+
+console.log ("4" + 5 + 6); //456 (string + number + number)= string
+
+console.log ("4" + "5" + "2"); //452 
+
+console.log (2**3**2); //2**(3**2) = 2**9 = 512 pahle 3**2=9 and second time 2**9 = 512
+
+console.log (3**2); //9
+
+console.log ("five"*2);  (2*"five"); //NaN
+
+console.log ("5"*2); // 10
+
+console.log (false==0); // true (== check only value)
+
+console.log (false===0); // false (=== check value and datatype)
+
+console.log(true+22); // 23 
+
+console.log ("2"*2);  //4
+
+let x;
+console.log(typeof x == "undefined") // true
+
+let y = "";
+console.log(typeof y == "undefined") // false
+
+// console.log(0.1+0.2===0.3) // false
+
+// console.log(2022012410463373819)
